0d1e11d3ddb231ea03e09a5d304e651e32923fb0
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-07-16 17:19:31 +0000

    Construccion de la solapa lateral y recortes varios

---
+++ bubbles/contenido/partes/boton-lateral.php
@@ -0,0 +1,11 @@
+<?php include('../../header.php') //SACAR EN LA DEFINITIVA!?>
+
+<div class="solapa-lateral">
+	<div class="extremo">
+	</div>
+	<div class="degrade">
+		<p>Mensajes</p>
+	</div>
+</div>
+
+<?php include('../../footer.php') //SACAR EN LA DEFINITIVA!?>
