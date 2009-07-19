8262e2a9f6720f7ae2f58c314d79f15e46a1aed4
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-07-19 00:52:44 +0000

    Casi completa la maqueta de un PERFIL

---
+++ bubbles/contenido/col-profesional-derecha.php
@@ -0,0 +1,14 @@
+<div class="col-profesional-derecha" >
+	<div class="superior-col">
+		<p>(poner imagen de cabecera)</p>
+	</div>
+	<div class="clasificados-col">
+		<p>agregar CLASIFICADOS segun criterio</p>
+	</div>
+	<div class="publicidad-col">
+	<p>agregar PUBLICIDAD</p>
+	</div>
+	<div class="inferior-col">
+	<p>(poner imagen de pie)</p>
+	</div>
+</div>
