2257280d379c6e320bf38fa08045e3193410ad4a
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-07-22 15:01:09 +0000

    Se dividio la columna central de un perfil profesional en parte superior e inferior

---
+++ bubbles/contenido/casilla-superior-profesional-mensajes.php
@@ -0,0 +1,19 @@
+<div class="casilla-superior">
+	<div class="borde-solapas">
+		<div class="solapa">
+			<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&portfolio=1"><p>Portfolio</p></a>
+		</div>
+		<div class="solapa">
+			<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&mensajes=1"><p>Mensajes</p></a>
+		</div>
+		<div class="solapa">
+		
+		</div>
+		<div class="solapa">
+		
+		</div>
+	</div>
+	<div class="mensajes">
+		<p>mensajes</p>
+	</div>
+</div>
