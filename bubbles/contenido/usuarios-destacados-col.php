a579574763e50e32918da10c07a98b7224be7921
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-07-30 06:45:43 +0000

    Maquetas-objeto del Home al 60%

--- bubbles/contenido/usuarios-destacados-col.php
+++ bubbles/contenido/usuarios-destacados-col.php
@@ -1,13 +1,11 @@
 <div class="usuarios-destacados-col">
 	<div class="cabecera">
-	<p>(insertar cabecera)</p>
 	</div>
 	<div class="superior">
-	<p>(insertar superior)</p>
 	</div>
 	<div class="contenido">
+	<p>(insertar contenido)</p>
 	</div>
 	<div class="inferior">
-	<p>(insertar inferior)</p>
 	</div>	
 </div>
