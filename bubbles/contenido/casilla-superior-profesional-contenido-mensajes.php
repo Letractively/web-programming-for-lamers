92a4c6d1183f707a9a1dd8a0747aa72eb4f62d84
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-08-04 03:34:48 +0000

    Se agregó exitosamente el front-end de "escribir mensaje privado" en los sitios de usuarios con prestaciones mce y entrega de mensajes en HTML

--- bubbles/contenido/casilla-superior-profesional-contenido-mensajes.php
+++ bubbles/contenido/casilla-superior-profesional-contenido-mensajes.php
@@ -1,12 +1,52 @@
+<script type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
+<script language="javascript" type="text/javascript">
+	tinyMCE.init({
+		mode : "textareas",
+		theme : "advanced",
+		theme_advanced_buttons1 : "bold, italic, underline, forecolor, backcolor, justifyleft, justifycenter, justifyright, link, unlink, bullist, numlist, undo, redo",
+		theme_advanced_buttons2 : "",
+		theme_advanced_buttons3 : "",
+		theme_advanced_toolbar_location : "top"
+	});
+</script>
+
 <div class="contenido-mensajes">
 	<div class="cabecera-escribir">
 	</div>
+	<form id="myform" action="">
 	<div class="datos-escribir">
+		<table>
+			<tr>
+				<th><p>Para:</p></th>
+				<td><input type="text" class="umPara" name="umPara" id="umPara" /></td>
+			</tr>
+			<tr>
+				<th><p>Asunto:</p></th>
+				<td><input type="text" class="umAsunto" name="umAsunto" id="umAsunto" /></td>
+			</tr>
+		</table>
 	</div>
-	<div class="mce-escribir">
-	</div>
+<!--<div class="mce-escribir">
+	</div>	-->
 	<div class="contenido-escribir">
+		<textarea id="umContenido" name="umContenido" rows="15" cols="80">
+		</textarea>
 	</div>
 	<div class="enviar-escribir">
+		<table>
+			<tr>
+				<td>
+				</td>
+				<td>
+				</td>
+				<td>
+				</td>
+				<td>
+					<input type="hidden" name="umMensajeEnviado" value="umMensajeEnviado" />
+					<input type="submit" class="boton3" name="Enviar" value="Enviar" />
+				</td>
+			</tr>
+		</table>
 	</div>
+	</form>
 </div>
