c0f4272423d935c0c725dab0f2d713408edd8d5d
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-08-07 20:03:49 +0000

    Se ordenaron las "partes" que arman el Todo de una página en diferentes carpetas que respetan la gerarquia porque si no el orden será imposible

---
+++ bubbles/contenido/casilla/superior/profesional/botones/nuevo-mensaje.php
@@ -0,0 +1,17 @@
+<?php //Botónes para mensaje abierto > ?>
+<div class="borde-botones-mensajes">
+	<table>
+	<tr>
+		<td><a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&solapa_superior=mensajes&botonera_superior=nuevo_mensaje&contenido_superior=nuevo_mensaje">
+			<input type="button" class="boton2" value="Nuevo" />
+			</a>
+		</td>
+	</tr>
+	<tr>
+		<td><input type="button" class="boton2" value="Respond." /></td>
+	</tr>
+	<tr>
+		<td><input type="button" class="boton2" value="Eliminar" /></td>
+	</tr>
+	</table>
+</div>
---
+++ bubbles/contenido/casilla/superior/profesional/contenido/casilla-entrada.php
@@ -0,0 +1,24 @@
+<div class="contenido-mensajes">
+	<div class="cabecera-escribir">
+		<table>
+		<tr>
+			<td>
+				<img src="imagenes/icono-bandeja-entrada.png"/>
+			</td>
+			<td>
+				<p class="parrafo4">Buzón de Entrada</p>
+			</td>
+			<td>
+				<img src="imagenes/icono-mensajes-enviados.png"/>
+			</td>
+			<td>
+				<p class="parrafo4">Elementos Enviados</p>
+			</td>
+		</tr>
+		</table>
+	</div>
+	<div class="lista-mensajes">
+	</div>
+	<div class="recorrer-mensajes">
+	</div>
+</div>
