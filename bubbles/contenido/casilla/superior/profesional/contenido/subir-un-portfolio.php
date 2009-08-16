86e4e6a4b31a1cbbf05a999a2598080dc4562a05
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-08-16 21:41:54 +0000

    "funcionalidades" del portfolio de un profesional casi completas

---
+++ bubbles/contenido/casilla/superior/profesional/contenido/subir-un-portfolio.php
@@ -0,0 +1,61 @@
+<?php
+$error_subiendo_portfolio = '';
+$nueva_muestra = new muestra();
+if(isset($_POST['subir_portfolio'])){
+	if(isset($_POST['nuevo'])){
+		if(0 < $_FILES['img1']['size'] && $_FILES['img1']['size'] < PESO_MAXIMO_PORTFOLIO && $_POST['titulo_portfolio'] != '' ){
+			//si ya se hizo clic en submit
+			$nueva_muestra->id_usuario = $visitado->id_usuario;
+			$nueva_muestra->titulo = myquery::cambiaTaMysql($_POST['titulo_portfolio']);
+			$nueva_muestra->comentario = myquery::cambiaTaMysql($_POST['comentario_portfolio']);
+			$nueva_muestra->guardarMuestra();
+			echo $nueva_muestra->ultimo_error;
+			$nueva_muestra->traerMuestrasCriterio(0, 1, $visitado->id_usuario);	//Busca la ULTIMA ENTRADA de este usuario para nombrar la foto:
+			echo $nueva_muestra->ultimo_error;
+			$nueva_muestra->id_muestra = $nueva_muestra->mu_id_muestra[0];
+			$fotos=imgResample2("img1", DIR_PORTFOLIOS_PROFESIONALES, $nueva_muestra->id_muestra , DIMENSION_PORTFOLIO_GRANDE, DIMENSION_PORTFOLIO_GRANDE, DIMENSION_PORTFOLIO_CHICO, DIMENSION_PORTFOLIO_CHICO);
+			$nueva_muestra->cargarRutaImagen($nueva_muestra->id_muestra . '.jpg');
+			echo $nueva_muestra->ultimo_error;
+		}
+		elseif(0 == $_FILES['img1']['size']){
+		$error_subiendo_portfolio = 'RUTA_INCORRECTA';
+		}
+		elseif($_POST['titulo_portfolio'] == ''){
+		$error_subiendo_portfolio = 'FALTA_TITULO';
+		}
+		else{
+		$error_subiendo_portfolio = 'PESO_INCORRECTO';
+		}
+	}
+	if(isset($_POST['eliminar'])){
+		$nueva_muestra->cargarRutaImagen('default.jpg'); //BORRAR el ROW correspondiente!!
+	}
+}
+?>
+
+<div class="contenido-portfolio">
+	<div class="cabecera-portfolio">
+		<h2><strong>Subir un nuevo trabajo</strong></h2>
+	</div>
+	<div class="imagenes-portfolio">
+	<form action="<?php echo $_SERVER['REQUEST_URI'];?>" method="POST" enctype="multipart/form-data" name="editpage" id="editpage">
+		<p class="parrafo8" style="color: #ff0000;">
+			<?php if($error_subiendo_portfolio == 'RUTA_INCORRECTA'){echo 'La ruta del archivo a subir es incorrecta!';} ?>
+			<?php if($error_subiendo_portfolio == 'PESO_INCORRECTO'){echo 'La foto a subir es demasido pesada!';} ?>
+			<?php if($error_subiendo_portfolio == 'FALTA_TITULO'){echo 'Debe poner un titulo al trabajo!';} ?>
+		</p>
+		<p class="parrafo8">Título:<input type="text" name="titulo_portfolio" class="boton2" /></p>
+				<p class="parrafo8">Breve Descripción:<input type="text" name="comentario_portfolio" class="boton2" /></p>
+		<input name="img1" type="file" id="img1" size="40">
+		<input type="hidden" name="subir_portfolio" value="subir_portfolio" />
+		<input type="submit" name="nuevo" class="boton2" value="Subir" />
+		<p class="parrafo8">Vista Previa:</p>
+		<img src="<?php echo DIR_PORTFOLIOS_PROFESIONALES_CHICOS . $nueva_muestra->ruta_imagen . '?' . rand(); ?>" />
+		<a href="profesional/<?php echo $_GET['entidad_visitada'] ?>">
+			<input type="button" name="aceptar" class="boton2" value="Aceptar" />
+		</a>
+	</form>
+	</div>
+	<div class="recorrer-portfolio">
+	</div>
+</div>
--- bubbles/contenido/casilla/superior/profesional/contenido/ver-portfolio.php
+++ bubbles/contenido/casilla/superior/profesional/contenido/ver-portfolio.php
@@ -1,14 +1,62 @@
+<?php
+$mi_portfolio = new muestra();
+$error_eliminando_portfolio = '';
+$mi_portfolio_offset = 0;
+$mi_portfolio_boton_siguiente = 'DESABILITADO';
+$mi_portfolio_boton_anterior = 'DESABILITADO';
+if(isset($_GET['eliminar_muestra_id']) && ((file_exists(DIR_PORTFOLIOS_PROFESIONALES_CHICOS . $_GET['eliminar_muestra_id'] . '.jpg')))
+	&& (file_exists(DIR_PORTFOLIOS_PROFESIONALES . $_GET['eliminar_muestra_id'] . '.jpg'))){
+	if ((!unlink(DIR_PORTFOLIOS_PROFESIONALES_CHICOS . $_GET['eliminar_muestra_id'] . '.jpg'))
+		|| (!unlink(DIR_PORTFOLIOS_PROFESIONALES . $_GET['eliminar_muestra_id'] . '.jpg'))){
+		$error_eliminando_portfolio = 'NO_SE_BORRARON_ARCHIVOS';
+	}
+	$mi_portfolio->borrarMuestra($_GET['eliminar_muestra_id']); //BORRAR el ROW correspondiente!!
+	echo $mi_portfolio->ultimo_error;
+}
+if(isset($_GET['mi_portfolio_offset']))
+{
+	$mi_portfolio_offset = $_GET['mi_portfolio_offset'];
+}
+$mi_portfolio->traerMuestrasCriterio($mi_portfolio_offset, 7, $visitado->id_usuario);
+echo $mi_portfolio->ultimo_error;
+?>
+
+<script type="text/javascript" src="jquery-fancybox/js/jquery.fancybox-1.2.1.js"></script>
+<link rel="stylesheet" href="jquery-fancybox/fancybox.css" type="text/css" media="screen">
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("a.muestra").fancybox();
+	});
+</script>
+
 <div class="contenido-portfolio">
 	<div class="cabecera-portfolio">
+		<h2><strong>Mi portfolio</strong></h2>
+		<p class="parrafo8" style="color: #ff0000;">
+			<?php if ($error_eliminando_portfolio == 'NO_SE_BORRARON_ARCHIVOS'){ echo 'no se pudieron eliminar los archivos!';} ?>
+			<?php if ($error_eliminando_portfolio == 'ARCHIVO_YA_ELIMINADO'){ echo 'el archivo ya ha sido eliminado!';} ?>
+		</p>
 	</div>
 	<div class="imagenes-portfolio">
 	<?php
 	$i=0;
-	for($i==0; $i<6; $i++){
+	if($mi_portfolio->ult_filas_afectadas > 6){
+		$mi_portfolio_boton_siguiente = 'HABILITADO';
+	}
+	if($mi_portfolio_offset != 0){
+		$mi_portfolio_boton_anterior = 'HABILITADO';
+	}
+	for($i==0; $i<($mi_portfolio->ult_filas_afectadas); $i++){
 		include('contenido/casilla/superior/profesional/contenido/un-portfolio-mio.php');
 	}
 	?>
 	</div>
 	<div class="recorrer-portfolio">
+		<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada']; ?>&solapa_superior=portfolio&botonera_superior=ver_portfolio&contenido_superior=ver_portfolio&mi_portfolio_offset=<?php echo $_GET['mi_portfolio_offset'] - 6 ?>">
+			<input type="button" <?php if($mi_portfolio_boton_anterior != 'HABILITADO'){ echo 'disabled = "TRUE"';}?> class="boton3" name="upSiguiente" value="Atras" />
+		</a>
+		<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada']; ?>&solapa_superior=portfolio&botonera_superior=ver_portfolio&contenido_superior=ver_portfolio&mi_portfolio_offset=<?php echo $_GET['mi_portfolio_offset'] + 6 ?>">
+			<input type="button" <?php if($mi_portfolio_boton_siguiente != 'HABILITADO'){ echo 'disabled = "TRUE"';}?> class="boton3" name="upSiguiente" value="Seguir" />
+		</a>
 	</div>
 </div>
