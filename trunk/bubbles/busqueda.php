2f985223adf2829622705c03631e95a477420d74
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-08-17 22:22:15 +0000

    -... nuevos recortes....

---
+++ trunk/bubbles/busqueda.php
@@ -0,0 +1,25 @@
+<?php include('header.php'); ?>
+<?php include('includes/clases/e_aviso.class.php'); ?>
+
+<div class="col-izquierda">
+	<p>col-izquierda</p>
+	<div class="col-busqueda-izquierda">
+		<p>col-busqueda-izquierda</p>
+	</div>
+</div>
+
+<div class="col-central">
+	<p>col-central</p>
+	<div class="col-busqueda-centro">
+		<p>col-busqueda-central</p>
+	</div>
+</div>
+
+<div class="col-derecha">
+	<p>col-derecha</p>
+	<div class="col-busqueda-derecha">
+		<p>col-busqueda-derecha</p>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
