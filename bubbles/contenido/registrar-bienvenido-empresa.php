a7b74dbf51a7e917d31290cb5f9fbe559997e963
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-08-25 18:34:29 +0000

    correnccion de BUGS de registro

--- bubbles/contenido/registrar-bienvenido-empresa.php
+++ bubbles/contenido/registrar-bienvenido-empresa.php
@@ -1,2 +1,2 @@
 <h2><?php echo $confirmaReg->alias_usuario; ?>, Bienvenido a la Comunidad Bubbles!!</h2>
-<h2><a href="e-perfil?entidad_visitada=<?php echo $confirmaReg->alias_usuario; ?>">VER MI PERFIL</a><h2>
+<h2><a href="empresa/<?php echo $confirmaReg->alias_usuario; ?>">VER MI PERFIL</a><h2>
