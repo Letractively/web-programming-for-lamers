b28705adb2f0070abb1f3bbc59a98bb9772c813b
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-01 03:35:15 +0000

    error estupido

--- bubbles/contenido/casilla/central/busqueda/un-profesional.php
+++ bubbles/contenido/casilla/central/busqueda/un-profesional.php
@@ -22,7 +22,7 @@ echo 'VISITANTE_ES: ' . $visitante_es;
 	
 	<div class="titulo-oferta-busqueda">
 		<p class="parrafo8" style="float: left; margin-left: 10px; margin-top: 0px; margin-bottom: 0px;">
-			<?php echo $este_usuario->alias; ?> (<?php myquery::cambiaTaNormal(echo $este_usuario->nombres); ?> <?php echo myquery::cambiaTaNormal($este_usuario->apellidos); ?>)
+			<?php echo $este_usuario->alias; ?> (<?php myquery::cambiaTaNormal($este_usuario->nombres); ?> <?php echo myquery::cambiaTaNormal($este_usuario->apellidos); ?>)
 		</p>
 		<p class="parrafo8" style="float: right; margin-right: 10px; margin-top: 0px; margin-bottom: 0px;">
 			Miembro desde: <?php echo myquery::cambiaFaNormal($este_usuario->miembro_desde); ?>
