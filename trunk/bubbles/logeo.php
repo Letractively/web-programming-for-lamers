3898b149e8066259e2352e70f827f411467725fe
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-01 18:45:08 +0000

    BUG: A partir de ahora, al estar logueado y deber loguearse de otra forma, la sesión anterior al nuevo logueo se MATA automaticamente al loguearse de nuevo.
    ... se continuan maquetas varias...

--- trunk/bubbles/logeo.php
+++ trunk/bubbles/logeo.php
@@ -7,6 +7,9 @@ require("includes/conectar_db.php");
 if(isset($_POST['usuario'])){
 	if(($id_usuario = ckeckLogeo( USUARIOS, $_POST['fAlias'], $_POST['fContrasenia'])) > -1 ) {
 		session_start();
+		$uri = $_SESSION['uri_rescatada'];
+		destruir_sesion_logeado();
+		session_start();
 		//session_register('logeado');	//Obsoleta
 		$_SESSION['hora'] = time();
 		$_SESSION['logeado'] = 1;
@@ -14,7 +17,6 @@ if(isset($_POST['usuario'])){
 		//$_SESSION['empresa'] = 0;
 		$_SESSION['id_usuario'] = $id_usuario;
 		//echo 'Ud se logueo como usuario <br />';
-		$uri = $_SESSION['uri_rescatada'];
 		header('Location:' . $uri);
 	}
 	else {
@@ -24,6 +26,9 @@ if(isset($_POST['usuario'])){
 if(isset($_POST['empresa'])){
 	if(($id_empresa = ckeckLogeo( EMPRESAS, $_POST['fAlias'], $_POST['fContrasenia'])) > -1 ) {
 		session_start();
+		$uri = $_SESSION['uri_rescatada'];
+		destruir_sesion_logeado();
+		session_start();
 		//session_register('logeado');	//Obsoleta
 		$_SESSION['hora'] = time();
 		$_SESSION['logeado'] = 1;
@@ -31,7 +36,6 @@ if(isset($_POST['empresa'])){
 		//$_SESSION['usuario'] = 0;
 		$_SESSION['id_empresa'] = $id_empresa;
 		//echo 'Ud se logueo como empresa <br />';
-		$uri = $_SESSION['uri_rescatada'];
 		header('Location:' . $uri);
 	} 
 	else {
