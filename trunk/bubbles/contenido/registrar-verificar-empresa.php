cf296457d62ae50820d4452725aceb57576d417a
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-12 20:45:18 +0000

    correción en el vinculo de confirmación

--- trunk/bubbles/contenido/registrar-verificar-empresa.php
+++ trunk/bubbles/contenido/registrar-verificar-empresa.php
@@ -7,9 +7,9 @@
 
 <?php
 //Preparo y envio el mail de aceptación al profesional...
-$codigohtml = '<html><head><title>' . $nuevoReg->alias_usuario . ', Bienvenido a Buubles!. Clickee en el siguiente vinculo para confirmar su postulacion:</title></head><body><a href=' . URL_BASE . '/registrar-nuevo-usuario.php?id=' . $nuevoReg->id_usuario . '&amp;reg=' . $nuevoReg->contrasenia . '">Verificar mi Memresia</a>"';
+$codigohtml = '<html><head><title>' . $nuevoReg->alias_usuario . ', Bienvenido a Buubles!. Clickee en el siguiente vinculo para confirmar su postulacion:</title></head><body><a href="' . URL_BASE . '/registrar-nuevo-usuario.php?id=' . $nuevoReg->id_usuario . '&amp;reg=' . $nuevoReg->contrasenia . '">Verificar mi Membresia</a>"';
 $email = $nuevoReg->email_usuario;
-$asunto = $nuevoReg->alias_usuario . ', bienvenido a BUBBLES. Confirme su membresia a continuación:';
+$asunto = $nuevoReg->alias_usuario . ', bienvenido a BUBBLES. Confirme su membresia';
 $cabeceras = "From: bubblescomunidad@bubblescomunidad.com\r\nContent-type: text/html\r\n";
 mail($email,$asunto,$codigohtml,$cabeceras);
 ?>
