18ea3c55ac5a5b9ffb8e98ea527113ec2b7041f7
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-13 02:39:24 +0000

    Se sigue trabajando para corregir los pasos de registro

--- trunk/bubbles/contenido/registrar-verificar-empresa.php
+++ trunk/bubbles/contenido/registrar-verificar-empresa.php
@@ -2,12 +2,17 @@
 <h2>Se ha enviado un mail de verificación a <?php echo $nuevoReg->email_usuario; ?> , confirme su membresia</h2>
 
 <p>El mail enviado debe redireccionar a:</p>
-<p>Bienvenido a Buubles! clickee en el siguiente vinculo para confirmar su postulacion;</p>
-<a href="<?php echo URL_BASE; ?>/registrar-nueva-empresa.php?id=<?php echo $nuevoReg->id_empresa ?>&amp;reg=<?php echo $nuevoReg->contrasenia_usuario; ?>">Verificar mi Memresia</a>
+<p>Bienvenido a Buubles! clickee en el siguiente vinculo para confirmar su postulacion:</p>
+<a href="<?php echo URL_BASE; ?>/registrar-nueva-empresa.php?id=<?php echo $nuevoReg->id_empresa ?>&amp;reg=<?php echo $nuevoReg->contrasenia_usuario; ?>">Verificar mi Membresia...</a>
 
 <?php
 //Preparo y envio el mail de aceptación al profesional...
-$codigohtml = '<html><head><title>' . $nuevoReg->alias_usuario . ', Bienvenido a Buubles!. Clickee en el siguiente vinculo para confirmar su postulacion:</title></head><body><a href="' . URL_BASE . '/registrar-nuevo-usuario.php?id=' . $nuevoReg->id_usuario . '&amp;reg=' . $nuevoReg->contrasenia . '">Verificar mi Membresia</a>"';
+$codigohtml = '<html><head><title>' . $nuevoReg->alias_usuario;
+$codigohtml .= ' Bienvenido a Bubbles</title></head><body>';
+$codigohtml .= 'Hola ' . $nuevoReg->alias_usuario . '!:<br />';
+$codigohtml .= 'Bienvenido a Bubbles!.<br />Clickee en el siguiente vinculo para confirmar su registro:<br />';
+$codigohtml .= '<a href="' . URL_BASE . '/registrar-nueva-empresa.php?id=' . $nuevoReg->id_empresa . '&amp;reg=' . $nuevoReg->contrasenia_usuario . '">Verificar mi Membresia...</a>';
+$codigohtml .= '</body></html>';
 $email = $nuevoReg->email_usuario;
 $asunto = $nuevoReg->alias_usuario . ', bienvenido a BUBBLES. Confirme su membresia';
 $cabeceras = "From: bubblescomunidad@bubblescomunidad.com\r\nContent-type: text/html\r\n";
