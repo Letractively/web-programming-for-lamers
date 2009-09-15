1d731b1a0885c1211e4ee70c68bb3081e8b58ab0
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-15 17:43:53 +0000

    Actualización de maquetas en todos los pasos de registro

--- bubbles/contenido/registrar-bienvenido-usuario.php
+++ bubbles/contenido/registrar-bienvenido-usuario.php
@@ -1,6 +1,8 @@
 <div class="col-central registro-verificado">
+	<div class="contenido-cartel">
+		<h2 class="al-medio"><?php echo $confirmaReg->alias; ?>, Bienvenido a la Comunidad Bubbles!!</h2>
+		<p class="parrafo8 al-medio">Su registro se completó exitosamente.</p>
+		<p class="parrafo8 al-medio">Logueese <a href="<?php echo URL_BASE; ?>">en su casilla correspondiente desde este vínculo</a> 
+			y podra entrar a su espacio personal; desde el link que se encuentra en la parte superior donde Ud. podra leer A MI PERFIL </p>
+	</div>
 </div>
-
-<h2><?php echo $confirmaReg->alias; ?>, Bienvenido a la Comunidad Bubbles!!</h2>
-<h2><a href="<?php echo URL_BASE; ?>">Su registro se completó exitosamente. 
-	Logueese en su casilla correspondiente y podra entrar a su perfil desde el vinculo que se encuentra en la parte superior de la página titulado A MI PERFIL...</a><h2>
--- bubbles/contenido/registrar-verificar-empresa.php
+++ bubbles/contenido/registrar-verificar-empresa.php
@@ -1,17 +1,20 @@
 <img src="imagenes/verificacion_mail.jpg" />
-<h2>Se ha enviado un mail de verificación a <?php echo $nuevoReg->email_usuario; ?> , confirme su membresia</h2>
+<h2 class="al-medio">Se ha enviado un mail de verificación a <?php echo $nuevoReg->email_usuario; ?>, confirme su membresia desde su casilla de e-mail</h2>
 
-<p>El mail enviado debe redireccionar a:</p>
-<p>Bienvenido a Buubles! clickee en el siguiente vinculo para confirmar su postulacion:</p>
-<a href="<?php echo URL_BASE; ?>/registrar-nueva-empresa.php?id=<?php echo $nuevoReg->id_empresa ?>&amp;reg=<?php echo $nuevoReg->contrasenia_usuario; ?>">Verificar mi Membresia...</a>
+<!--<p>El mail enviado debe redireccionar a:</p>
+<p>Bienvenido a Buubles! clickee en el siguiente vinculo para confirmar su postulacion:</p>-->
+<!--<a href="<?php //echo URL_BASE; ?>/registrar-nueva-empresa.php?id=<?php //echo $nuevoReg->id_empresa ?>&amp;reg=<?php //echo $nuevoReg->contrasenia_usuario; ?>">Verificar mi Membresia...</a>-->
 
 <?php
-//Preparo y envio el mail de aceptación al profesional...
+//Preparo y envio el mail de verificación...
 $codigohtml = '<html><head><title>' . $nuevoReg->alias_usuario;
 $codigohtml .= ' Bienvenido a Bubbles</title></head><body>';
 $codigohtml .= 'Hola ' . $nuevoReg->alias_usuario . '!:<br />';
 $codigohtml .= 'Bienvenido a Bubbles!.<br />Clickee en el siguiente vinculo para confirmar su registro:<br />';
-$codigohtml .= '<a href="' . URL_BASE . '/registrar-nueva-empresa.php?id=' . $nuevoReg->id_empresa . '&amp;reg=' . $nuevoReg->contrasenia_usuario . '">Verificar mi Membresia...</a>';
+$codigohtml .= '<a href="' . URL_BASE . '/registrar-nueva-empresa.php?id=' . $nuevoReg->id_empresa . '&amp;reg=' . $nuevoReg->contrasenia_usuario . '">Verificar mi Membresia...</a><br />';
+$codigohtml .= 'Te recordamos los datos de tu cuenta para que puedas loguearte:<br />';
+$codigohtml .= '- tu usuario es: ' . $nuevoReg->alias_usuario . '<br />';
+$codigohtml .= '- tu password es: ' . $_SESSION['feContraseniaUsuario'] . '<br />';
 $codigohtml .= '</body></html>';
 $email = $nuevoReg->email_usuario;
 $asunto = $nuevoReg->alias_usuario . ', bienvenido a BUBBLES. Confirme su membresia';
