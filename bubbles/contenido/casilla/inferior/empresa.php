0d8440653118168768a2a7ccd53ccf3505bc5260
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-20 01:30:24 +0000

    AGREGADO: la acción de comentar es avisada via mail a la EMPRESA afectada

--- bubbles/contenido/casilla/inferior/empresa.php
+++ bubbles/contenido/casilla/inferior/empresa.php
@@ -6,15 +6,30 @@ if(isset($_POST['ecComentar'])){
 		if(isset($_SESSION['id_usuario'])){
 			$este_comentario->desde_entidad = 'PROFESIONAL';
 			$este_comentario->id_desde = $_SESSION['id_usuario'];
+			// Levanto los datos básicos de la amistad agregada para saber a quien envio el comentario:
+			$este_comentador_datos = new usuario($este_comentario->id_desde);
+			$este_comentador_alias = $este_comentador_datos->alias;
 		}elseif(isset($_SESSION['id_empresa'])){
 			$este_comentario->desde_entidad = 'EMPRESA';
 			$este_comentario->id_desde = $_SESSION['id_empresa'];
+			// Levanto los datos básicos de la amistad agregada para saber a quien envio el comentario:
+			$este_comentador_datos = new empresa($este_comentario->id_desde);
+			$este_comentador_alias = $este_comentador_datos->alias_usuario;
 		}
 		$este_comentario->para_entidad = 'EMPRESA';
 		$este_comentario->id_para = $visitado->id_empresa;
 		$este_comentario->detalle = myquery::cambiaTaMysql($_POST['ecDetalle']);
 		$este_comentario->guardarComentario();
 		echo $este_comentario->ultimo_error;
+		
+		// Avisar a "$este_visitado" de que le ha dejado un comentario otra entidad....
+		$codigohtml = $este_comentador_alias . ' te ha dejado un comentario:<br />';
+		$codigohtml .= "<html><head><title></title></head><body><a href=\"" . URL_BASE . "e-galeria.php?entidad_visitada=" . $visitado->alias_usuario . "&casilla_central=todos_comentarios&contenido_central=todos_comentarios\">Haz click aqui para ver el comentario</a><br />";
+		$codigohtml .= '</html></body>';
+		$email = $visitado->email_usuario;
+		$asunto = 'Tienes un nuevo comentario';
+		$cabeceras = "From: bubblescomunidad@bubblescomunidad.com\r\nContent-type: text/html\r\n";
+		mail($email,$asunto,$codigohtml,$cabeceras);
 	}else{
 		$error_insertando_comentario = 'CONTENIDO_VACIO';
 	}	
