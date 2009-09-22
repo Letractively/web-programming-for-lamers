109fa13d918dc2553f3b8d31913c36d69dc81027
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2009-09-22 22:01:33 +0000

    mejoramiento de colores y perfiles curvos

--- bubbles/contenido/casilla/superior/profesional/solapas/portfolio.php
+++ bubbles/contenido/casilla/superior/profesional/solapas/portfolio.php
@@ -20,21 +20,20 @@ $leyenda_solapa_mensaje = 'Dejar msj.';
 //	};
 
 </script>
-
 	<div class="borde-solapas">
-		<div class="solapa">
+	<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&solapa_superior=portfolio&botonera_superior=ver_portfolio&contenido_superior=ver_portfolio">
+			<div class="solapa">
 				<!-- onclick="cargarPortfolio(); return(false);" -->
-			<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&solapa_superior=portfolio&botonera_superior=ver_portfolio&contenido_superior=ver_portfolio">
 				<img src="imagenes/icono-portfolio.png" style="position: absolute; margin-top: 15px; margin-left: 20px; border: 0;" />
-			</a>
 			<p class="parrafo3" style="position: absolute; margin-top: 60px; margin-left: 20px; border: 0;">Portfolio</p>
 		</div>
+	</a>
+	<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&solapa_superior=mensajes&botonera_superior=nuevo_mensaje&contenido_superior=<?php echo $contenido_superior_solapa_mensajes ?>">
 		<div class="solapa">
-			<a href="u-galeria.php?entidad_visitada=<?php echo $_GET['entidad_visitada'] ?>&solapa_superior=mensajes&botonera_superior=nuevo_mensaje&contenido_superior=<?php echo $contenido_superior_solapa_mensajes ?>">
 				<img src="imagenes/icono-mensajes.png" style="position: absolute; margin-top: 15px; margin-left: 20px; border: 0;" />
-			</a>
 			<p class="parrafo3" style="position: absolute; margin-top: 60px; margin-left: 20px; border: 0;"><?php echo $leyenda_solapa_mensaje; ?></p>
 		</div>
+	</a>
 		<div class="solapa">
 		
 		</div>
