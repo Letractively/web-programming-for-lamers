2838a6d39c42d1810c41073ff9f26a3eabba3b84
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2010-06-05 23:04:26 +0000

    Cabio de dos imágenes por cuatro DIVS (en la intro) terminada

--- trunk/sevsaservicios/index.php
+++ trunk/sevsaservicios/index.php
@@ -16,17 +16,51 @@
     Released by Jon Raasch (jonraasch.com) under FreeBSD license: free to use or modify, not responsible for anything, etc.  Please link out to me if you like it :)
 ***/
 
-function cambiarImagen() {
+function cambiarImagen1() {
 
 	var img_numero = '1';
-	
 	var $img_activa = $('.foto_intro_' + img_numero + ' IMG.imgs_intro_1');
+
+	if ( $img_activa.length == 0 ){ 
+		$img_activa = $('.foto_intro_1 IMG:last');
+	}
+	
+	var $next =  $img_activa.next().length ? $img_activa.next() : $('.foto_intro_1 IMG:first');
+	
+	
 	$img_activa.animate({"opacity": 0.0}, 1000);
-	$img_activa.animate({"opacity": 1.0}, 1000);
+	//$img_activa.animate({"opacity": 1.0}, 1000);
+}
+
+function cambiarImagen2() {
+	var img_numero = '2';
+	var $img_activa = $('.foto_intro_' + img_numero + ' IMG.imgs_intro_1');
+	
+	$img_activa.animate({"opacity": 0.0}, 1000);
+	//$img_activa.animate({"opacity": 1.0}, 1000);
+}
+
+function cambiarImagen3() {
+	var img_numero = '3';
+	var $img_activa = $('.foto_intro_' + img_numero + ' IMG.imgs_intro_1');
+	
+	$img_activa.animate({"opacity": 0.0}, 1000);
+	//$img_activa.animate({"opacity": 1.0}, 1000);
+}
+
+function cambiarImagen4() {
+	var img_numero = '4';
+	var $img_activa = $('.foto_intro_' + img_numero + ' IMG.imgs_intro_1');
+	
+	$img_activa.animate({"opacity": 0.0}, 1000);
+	//$img_activa.animate({"opacity": 1.0}, 1000);
 }
 
 $(function() {
-		setInterval( "cambiarImagen()", 3000 );
+		setTimeout( "cambiarImagen1()", 3000 );
+		setTimeout( "cambiarImagen2()", 6000 );
+		setTimeout( "cambiarImagen3()", 9000 );
+		setTimeout( "cambiarImagen4()", 12000 );
 });
 </script>
 
@@ -38,8 +72,7 @@ $(function() {
 	
 	<div id="main-container">
 
-		<img id="logo-intro" src="images/logo_sevsa.png">
-		</img>
+		<img id="logo-intro" src="images/logo_sevsa.png" />
 	
 		<div class="foto_intro_1">
 			<img src="images/thumb_1.jpg" class="imgs_intro_1" />
