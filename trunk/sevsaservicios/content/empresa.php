9dbcd44af0a987b03ec84b48140acdc6c158ec0d
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2010-06-11 15:03:54 +0000

    agregado de metodos AJAX para cargar las diferentes solapas laterales. Imágenes nuevas.

---
+++ trunk/sevsaservicios/content/empresa.php
@@ -0,0 +1,20 @@
+<div id="content_upper">
+	<h1 class="titulo_cont">EMPRESA</h1>
+</div>
+<div id="content_lower">
+	<p><strong>SEV-S.A,</strong> Servicios Eléctricos del Valle es una empresa de Servicios  constituida en 1986, 
+	nuestra basta experiencia nos ha permitido expandirnos consolidadamente, aumentando proporcionalmente 
+	nuestra cantidad de servicios con el objetivo de brindar una respuesta responsable a cada una de 
+	las necesidades del medio ambiente, mercado y cada uno de nuestros clientes.</p>
+	<p>Actualmente prestamos servicios en la industria frutícola, minera y la del petróleo; 
+	nos hemos especializado en Montajes Eléctricos, Obras Civiles, Recuperación de Cañerias en General, 
+	Trabajos de Soldaduras Industriales, Saneamiento  Ambiental, Movimiento de Suelos y  Piping.</p>
+	<p>Como empresa hacemos énfasis en la calidad humana, en crear una ambiente laboral confortable y 
+	por sobre todo cuidamos la seguridad de nuestro personal, por ello trabajamos en condiciones Seguras 
+	para lo cuál se dispone de personal de seguridad en la Obra con alta calidad en sus competencias, 
+	ocupándonos constantemente de su desarrollo profesional. Para esto y, en alianza estratégica con 
+	DamecyPro Services nos ocupamos también, de su capacitación en cada una de las áreas en las que se desarrollan 
+	todos nuestros colaboradores. Además, convencidos de que las condiciones sociales y personales hacen al marco 
+	de las mismas, el objeto de esta empresa es inculcar sobre todo valores propios que hagan de las personas 
+	verdaderos profesionales.</p>
+</div>
---
+++ trunk/sevsaservicios/content/equipamiento.php
@@ -0,0 +1,1 @@
+<h1 class="titulo_cont">EQUIPAMIENTO</h1>
