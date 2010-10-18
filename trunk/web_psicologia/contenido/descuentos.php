93271aaa65bdf28bd5f703681584b7f48022095f
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2010-10-18 19:32:17 +0000

    php queryes e imágenes originales completo

---
+++ trunk/web_psicologia/contenido/descuentos.php
@@ -0,0 +1,49 @@
+<!--Modulo de Descuentos -->
+<p id="titpag">
+	Descuentos
+</p>
+<div id="conten">
+	<table style="width: 90%; margin: 40px auto 20px 20px;">
+		<tbody><tr>
+			<td style="width: 50%; background-color: rgb(238, 238, 238); vertical-align: top; padding-top: 10px; border: 1px solid rgb(255, 255, 255);">
+				<div style="position: relative; width: 320px; font-size: 13pt; margin: 0pt auto;">
+					<div style="margin-left: 20px;">
+						<p>Tarifas reducidas para ...</p>
+						<ul style="list-style-image: url(&quot;images/bolo.gif&quot;);">
+							<li>Estudiantes</li>
+							<li>Parados</li>
+							<li>Madres solteras</li>
+							<li>Jubilados</li>
+							<li>Familias numerosas</li>
+						</ul>
+					</div>
+					<p style="margin-bottom: 10px;" class="promo">Descuento de 10 € en cada sesión</p>
+					<ul style="text-align: justify; background: none repeat scroll 0% 0% rgb(254, 254, 254); font-size: 10pt; padding: 10px 15px 15px;">
+						<li style="margin-left: 5px;">No es acumulable la concurrencia de más de una situación</li>
+						<li style="margin-left: 5px;">No se podrán aplicar estos descuentos en las modalidades <span class="ital">Ejecutivo y Personal In Situ</span>.</li>
+					</ul>
+					<br>
+				</div>
+			</td>
+			<td style="width: 50%;">
+				<div style="position: relative; width: 100%; margin: 15px auto 0pt 10px; padding: 0px 10px;">
+					<div class="adorned">
+						<p style="margin: 0pt auto; color: red; font-size: 130%; font-weight: bold; text-align: center;">«&nbsp;DESCUENTO POR DIFUSIÓN&nbsp;»</p>
+						<hr style="margin: 10px auto; height: 1px; width: 98%; background-color: rgb(255, 255, 255);">
+						<p>Ahora, por cada persona que venga de tu parte, tienes derecho a un descuento de 5 €.</p>
+						<p>Puedes acumular estos descuentos hasta un máximo de 20 € (4 personas).</p>
+						<p>Te podrás beneficiar de este descuento durante el tiempo que la persona que has recomendado este realizando su PCP.</p>
+						<p>Puedes acumular este descuento a otros descuentos.</p>
+					</div>
+				</div>
+			</td>
+		</tr>
+		<tr><td colspan="2"><br><hr style="width: 90%; margin: 0pt auto;"><br></td></tr>
+		<tr>
+			<td style="text-align: center;" colspan="2">
+				Puedes ampliar esta información y consultar todos los descuentos <span style="position: relative; display: inline; border: 1px solid rgb(170, 170, 170); text-align: center; height: 20px;" class="defini"><a target="_blank" href="index.php?itemid=75">&nbsp;&nbsp;Pulsando aquí&nbsp;&nbsp;</a></span>
+			</td>
+		</tr>
+	</tbody></table>
+</div>
+<!--Fin del Modulo de Descuentos -->
