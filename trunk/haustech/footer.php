d0712bb1cdb02c58e929755d94a68b367393a0d5
david.ercoli <david.ercoli@176d8340-b4c9-11dd-8f72-659bbef264a5>
2010-12-08 18:35:45 +0000

    estilos menores y logo en footer.php

--- trunk/haustech/footer.php
+++ trunk/haustech/footer.php
@@ -1,5 +1,5 @@
         <div id="footer">
-        	<div id="footer_ctnt">
+        	<div id="footer_ctnt">            <a href="http://www.control4.com"><img class="control4logo" src="img/control4logo.jpg" /></a>			
             <a href="#"><img src="img/footer_top.gif" /></a>
             </div>
         </div>
