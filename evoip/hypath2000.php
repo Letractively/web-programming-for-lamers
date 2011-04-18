a3a4bdd906c06d494442640090ab1eff4e753d0a
david.ercoli <david.ercoli@gmail.com>
2011-04-18 22:20:47 +0000

    Adhesión de nuevos productos.

---
+++ evoip/hypath2000.php
@@ -0,0 +1,70 @@
+<?
+	
+	//Link activo en header
+	$active = "productos";
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<?
+		include "header.php"
+	?>	
+    <div id="bloque_general">
+    	
+        <div id="bloque_general_cntn">
+        	
+            <div id="gradient_light">
+        		
+                <span class="title">Productos</span>
+                
+                <ul id="link_list">
+                	<li><a href="productosVoz.php" class="active">Voz</a></li>                    <li><a href="productosImagen.php">Imagen</a></li>
+                </ul>
+        	
+            </div>
+            
+            <div id="box_productos">
+                
+                <div id="box_left">
+                	
+                    <h1>Hi Path 2000</h1>
+                    
+                    <p>Para la movilidad de voz y datos, existen opciones para desplegar tecnología WLAN.</p>
+
+					<p>HiPath 2000 permite realizar una red corporativa como red integrada para voz y datos. Las infraestructuras existentes pueden actualizarse para el soporte de comunicaciones IP en tiempo real, como la voz, con lo que incluso en pequeñas redes LAN, pueden aprovecharse las ventajas de la Telefonía IP</p>															<p>El nuevo sistema permite flexibilidad y movilidad en las comunicaciones de oficina. Por medio de software integrado para cifrado, puede disponerse de una Red Privada Virtual (VPN), con lo que los teletrabajadores y usuarios en "oficinas satélite" pueden conectarse de forma segura a la red de la compañía. Así el personal que trabaja en modo remoto a travéz de DSL, puede también utilizar el teléfono como si se encontrara en la oficina.</p>															<p>En este sentido HiPath 2000 soporta los puntosde acceso de Siemens, por medio de los cuales se puede tener transmisión de voz sobre la WLAN. también integra función de Voice-Mail, con la que se puede disponer de hasta 24 buzones para mensajes. Para la comunicación telefónica, Siemens dispone de una variedad de teléfonos IP, teléfonos software y terminales WLAN.</p>
+                    
+                </div>
+                
+                <div id="box_right">
+                
+                	<img src="img/Hypat2000.png" />
+                    
+                    <ul>
+                    	<li>Gateway interfaz hacia red pública con enlaces RDSI.</li>
+                        <li>Firewall integrado.</li>
+                        <li>Conexiones LAN/WAN para ISP de Internet.</li>
+                        <li>Basado en SIP, sistema abierto a servicios de operadores y proveedores.</li>												<li>Switch LAN para redes de PC.</li>
+                    </ul>
+                
+                </div>
+            
+            </div>
+                        
+    	</div>
+    
+    </div>
+    
+<?
+
+	include "footer.php";
+
+?>
+</body>
+</html>
---
+++ evoip/hypath3000.php
@@ -0,0 +1,70 @@
+<?
+	
+	//Link activo en header
+	$active = "productos";
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<?
+		include "header.php"
+	?>	
+    <div id="bloque_general">
+    	
+        <div id="bloque_general_cntn">
+        	
+            <div id="gradient_light">
+        		
+                <span class="title">Productos</span>
+                
+                <ul id="link_list">
+                	<li><a href="productosVoz.php" class="active">Voz</a></li>                    <li><a href="productosImagen.php">Imagen</a></li>
+                </ul>
+        	
+            </div>
+            
+            <div id="box_productos">
+                
+                <div id="box_left">
+                	
+                    <h1>Hi Path 3000</h1>
+                    
+                    <p>Una plataforma flexible de comunicaciones para pequeñas y medianas empresas que se amplia hasta 500 usuarios.</p>
+
+					<p>HiPath 3000 es una plataforma modular de comunicaciones que ofrece un conjunto de funciones lider en la industria para pequeñas y medianas empresas de hasta 500 usuarios. Además, con soporte para cualquier combinación de teléfonos TDM, analógicos e IP, clientes PC y teléfonos inalámbricos, es ideal para entornos heterogéneos.</p>															<p>La conexión en red de varios sitios permite a las empresas con sitios remotos operarlo como un solo sistema. Reducción de costos de llamadas al usar telefonía IP entre sitios o Internet. acceso a funciones de telefonía líderes de la industria en entornos TDM o IP</p>															<p>Además de desplegar una amplia variedad de opciones de conectividad y plataforma, HiPath 3000 también ofrece varios módulos que mejoran significativamente la capacidad de comunicaciones de la solución: Diseñado especificamente para pequeñas y medianas empresas, OpenScape Office ayuda a las organizaciones a trabajar de manera más productiva, mejorar el servicio al cliente y ahorrar dinero.</p>															<p>Xpressions Compact mejora la mensajería genérica de voz de la telefonía en la oficina al proporcionar un buzón de correo de voz unificado para los teléfonos móviles y fijos de los usuarios, permitirles recibir mensajes de correo de voz por correo electrónico, registrar llamadas y mucho más.</p>															<p>HG1500 Gateway Card para comunicaciones IP y conectividad de banda ancha.</p>
+                    
+                </div>
+                
+                <div id="box_right">
+                
+                	<img src="img/Hypat3000.png" />
+                    
+                    <ul>
+                    	<li>ISDN2e y líneas analógicas.</li>
+                        <li>Interfaz CSTA.</li>
+                        <li>Administración vía TCP/IP.</li>
+                        <li>Mantenimiento Remoto (vía ISDN o análogo).</li>												<li>Establecimiento de Red IP (tarjeta opcional HG1500).</li>												<li>Microteléfonos IP (tarjeta opcional HG1500).</li>
+                    </ul>
+                
+                </div>
+            
+            </div>
+                        
+    	</div>
+    
+    </div>
+    
+<?
+
+	include "footer.php";
+
+?>
+</body>
+</html>
