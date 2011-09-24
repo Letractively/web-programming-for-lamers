e1d24cb755769506930eeaf1d0ac4dcc248148ef
david.ercoli@gmail.com <david.ercoli@gmail.com@176d8340-b4c9-11dd-8f72-659bbef264a5>
2011-09-24 14:31:41 +0000

    Se agrego GAnalytics a todos los headers

--- trunk/evoip/videoVigilanciaIP.php
+++ trunk/evoip/videoVigilanciaIP.php
@@ -9,8 +9,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Evoip - Soluciones Tecnológicas</title>
-<link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
+<link href="css/style.css" rel="stylesheet" type="text/css"><script type="text/javascript">  var _gaq = _gaq || [];  _gaq.push(['_setAccount', 'UA-25920886-1']);  _gaq.push(['_trackPageview']);  (function() {    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();</script>
+</head>
 
 <body>
 	<?
