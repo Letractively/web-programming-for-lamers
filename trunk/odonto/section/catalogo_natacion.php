90c2f87af8cdf95dfd0315854918424acb81a59e
david.ercoli@gmail.com <david.ercoli@gmail.com@176d8340-b4c9-11dd-8f72-659bbef264a5>
2012-02-24 12:04:32 +0000

    Versión nueva de Odonto

---
+++ trunk/odonto/section/catalogo_natacion.php
@@ -0,0 +1,373 @@
+<?php 
+	if (!$included) die();
+?>
+
+        
+        <div style="margin:10px 45px 20px 20px;">
+            <div style="float:left;">
+                <h1 style="color:#bebebe;"> Catálogo </h1>
+                <h3>Natación</h3>
+            </div>
+
+			<!-- gallerific start -->       
+            
+	            <div id="gallery" class="content">
+					<!-- <div id="controls" class="controls"></div> -->
+					<div class="slideshow-container">
+						<div id="loading" class="loader"></div>
+						<div id="slideshow" class="slideshow"></div>
+					</div>
+					<div id="caption" class="caption-container"></div>
+				</div>
+                
+				<div id="thumbs" class="navigation" style="float:left; width:350px;">
+					<ul class="thumbs noscript">
+						<li>
+							<a class="thumb" href="img/gal/foto1.jpg" title="Vestido de Danza 1">
+								<img src="img/gal/foto1-thumb.jpg" alt="Vestido de Danza 1" />
+							</a>
+							<div class="caption">
+								<div class="image-title">Vestido de Danza 1</div>
+								<div class="image-desc">
+                                	Lorem ipsum dolor sic amet consectetur adepescit et astra in dolorem natura. 
+	                                In sic torrentes perspitia presentum ar nosber.
+                                    <br />
+                                    <ul>
+	                                    <li>Distancia de pierna 27cm.</li>
+									    <li>50% polyester, 50% polybutyleno.</li>
+                                    </ul>
+                                    
+									<b>Colección:</b> Nubia.<br />
+									<b>Etc:</b> Etc.<br />
+									
+                                    <div style="border-top:1px solid #ccc; margin:10px 0 0 0; padding:10px 0 0 0;">
+										<b>Colores disponibles:</b><br />
+                                        
+                                        <div class="colorblock" style="background-color:#1f1a17;"></div>
+                                        <div class="colorblock" style="background-color:#27283c;"></div>
+                                        <div class="colorblock" style="background-color:#266cbc;"></div>
+                                        <div class="colorblock" style="background-color:#d03628;"></div>
+                                        <div style="clear:both; height:10px;"></div>
+                                        
+										<b>Tamaños:</b><br />
+                                        
+                                        <div class="sizeblock">28</div>
+                                        <div class="sizeblock">30</div>
+                                        <div class="sizeblock">32</div>
+                                        <div class="sizeblock">34</div>
+                                        <div class="sizeblock">36</div>
+                                        <div class="sizeblock">38</div>
+                                        <div class="sizeblock">40</div>
+                                        
+                                        <div style="clear:both; height:10px;"></div>
+                                    </div>
+                                    
+                                </div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" name="drop" href="http://farm3.static.flickr.com/2404/2538171134_2f77bc00d9.jpg" title="Title #1">
+								<img src="http://farm3.static.flickr.com/2404/2538171134_2f77bc00d9_s.jpg" alt="Title #1" />
+							</a>
+							<div class="caption">
+								Any html can be placed here ...
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" name="bigleaf" href="http://farm3.static.flickr.com/2093/2538168854_f75e408156.jpg" title="Title #2">
+								<img src="http://farm3.static.flickr.com/2093/2538168854_f75e408156_s.jpg" alt="Title #2" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2093/2538168854_f75e408156_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #2</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" name="lizard" href="http://farm4.static.flickr.com/3153/2538167690_c812461b7b.jpg" title="Title #3">
+								<img src="http://farm4.static.flickr.com/3153/2538167690_c812461b7b_s.jpg" alt="Title #3" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3153/2538167690_c812461b7b_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #3</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3150/2538167224_0a6075dd18.jpg" title="Title #4">
+								<img src="http://farm4.static.flickr.com/3150/2538167224_0a6075dd18_s.jpg" alt="Title #4" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3150/2538167224_0a6075dd18_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #4</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3204/2537348699_bfd38bd9fd.jpg" title="Title #5">
+								<img src="http://farm4.static.flickr.com/3204/2537348699_bfd38bd9fd_s.jpg" alt="Title #5" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3204/2537348699_bfd38bd9fd_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #5</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3124/2538164582_b9d18f9d1b.jpg" title="Title #6">
+								<img src="http://farm4.static.flickr.com/3124/2538164582_b9d18f9d1b_s.jpg" alt="Title #6" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3124/2538164582_b9d18f9d1b_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #6</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3205/2538164270_4369bbdd23.jpg" title="Title #7">
+								<img src="http://farm4.static.flickr.com/3205/2538164270_4369bbdd23_s.jpg" alt="Title #7" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3205/2538164270_c7d1646ecf_o.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #7</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3211/2538163540_c2026243d2.jpg" title="Title #8">
+								<img src="http://farm4.static.flickr.com/3211/2538163540_c2026243d2_s.jpg" alt="Title #8" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3211/2538163540_c2026243d2_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #8</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2315/2537343449_f933be8036.jpg" title="Title #9">
+								<img src="http://farm3.static.flickr.com/2315/2537343449_f933be8036_s.jpg" alt="Title #9" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2315/2537343449_f933be8036_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #9</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2167/2082738157_436d1eb280.jpg" title="Title #10">
+								<img src="http://farm3.static.flickr.com/2167/2082738157_436d1eb280_s.jpg" alt="Title #10" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2167/2082738157_436d1eb280_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #10</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2342/2083508720_fa906f685e.jpg" title="Title #11">
+								<img src="http://farm3.static.flickr.com/2342/2083508720_fa906f685e_s.jpg" alt="Title #11" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2342/2083508720_fa906f685e_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #11</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2132/2082721339_4b06f6abba.jpg" title="Title #12">
+								<img src="http://farm3.static.flickr.com/2132/2082721339_4b06f6abba_s.jpg" alt="Title #12" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2132/2082721339_4b06f6abba_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #12</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2139/2083503622_5b17f16a60.jpg" title="Title #13">
+								<img src="http://farm3.static.flickr.com/2139/2083503622_5b17f16a60_s.jpg" alt="Title #13" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2139/2083503622_5b17f16a60_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #13</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2041/2083498578_114e117aab.jpg" title="Title #14">
+								<img src="http://farm3.static.flickr.com/2041/2083498578_114e117aab_s.jpg" alt="Title #14" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2041/2083498578_114e117aab_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #14</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2149/2082705341_afcdda0663.jpg" title="Title #15">
+								<img src="http://farm3.static.flickr.com/2149/2082705341_afcdda0663_s.jpg" alt="Title #15" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2149/2082705341_afcdda0663_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #15</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2014/2083478274_26775114dc.jpg" title="Title #16">
+								<img src="http://farm3.static.flickr.com/2014/2083478274_26775114dc_s.jpg" alt="Title #16" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2014/2083478274_26775114dc_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #16</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2194/2083464534_122e849241.jpg" title="Title #17">
+								<img src="http://farm3.static.flickr.com/2194/2083464534_122e849241_s.jpg" alt="Title #17" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2194/2083464534_122e849241_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #17</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm4.static.flickr.com/3127/2538173236_b704e7622e.jpg" title="Title #18">
+								<img src="http://farm4.static.flickr.com/3127/2538173236_b704e7622e_s.jpg" alt="Title #18" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm4.static.flickr.com/3127/2538173236_b704e7622e_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #18</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2375/2538172432_3343a47341.jpg" title="Title #19">
+								<img src="http://farm3.static.flickr.com/2375/2538172432_3343a47341_s.jpg" alt="Title #19" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2375/2538172432_3343a47341_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #19</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2353/2083476642_d00372b96f.jpg" title="Title #20">
+								<img src="http://farm3.static.flickr.com/2353/2083476642_d00372b96f_s.jpg" alt="Title #20" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2353/2083476642_d00372b96f_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #20</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm3.static.flickr.com/2201/1502907190_7b4a2a0e34.jpg" title="Title #21">
+								<img src="http://farm3.static.flickr.com/2201/1502907190_7b4a2a0e34_s.jpg" alt="Title #21" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm3.static.flickr.com/2201/1502907190_7b4a2a0e34_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #21</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+						<li>
+							<a class="thumb" href="http://farm2.static.flickr.com/1116/1380178473_fc640e097a.jpg" title="Title #22">
+								<img src="http://farm2.static.flickr.com/1116/1380178473_fc640e097a_s.jpg" alt="Title #22" />
+							</a>
+							<div class="caption">
+								<div class="download">
+									<a href="http://farm2.static.flickr.com/1116/1380178473_fc640e097a_b.jpg">Download Original</a>
+								</div>
+								<div class="image-title">Title #22</div>
+								<div class="image-desc">Description</div>
+							</div>
+						</li>
+
+					</ul>
+				</div>
+				<div style="clear: both;"></div>
+            
+            <!-- gallerific end -->
+
+            
+        </div>
+        
+        
+        
+        <div style="clear:both; height:40px;"></div>
+
+        
+                
+                                
+                
+                
+
+                
+                
+                
+           
---
+++ trunk/odonto/section/servicios.php
@@ -0,0 +1,179 @@
+<?php 
+	if (!$included) die();
+?>
+
+
+
+<div id="headtitle"> 
+	<h1>Servicios</h1>
+</div>
+
+<div id="topParagraph">
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Donec viverra odio sit amet lectus hendrerit malesuada dapibus nulla scelerisque. 
+    iquam purus turpis, aliquet id vestibulum vitae, consequat at erat.</p>
+</div>
+
+
+
+
+
+
+<div class="block1">
+	<div class="servicio">
+        <img src="img/block/ortodoncia.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Ortodoncia</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>
+    </div>
+</div>
+
+<div class="block2">
+	<div class="servicio">
+        <img src="img/block/implantes.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Implantes Dentales</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>    
+    </div>         
+</div>
+
+<div class="block3">
+	<div class="servicio">
+        <img src="img/block/terapia.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Terapia Preodontológica</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>     
+    </div>        
+</div>
+
+
+
+
+<div style="clear:both; height:30px;"> </div>
+
+
+
+              
+                
+<div class="block1">
+	<div class="servicio">
+        <img src="img/block/blanqueamiento.jpg" /> <br />
+        <h3 style="margin:6px 0 0 0px;">Blanqueamiento</h3>
+        <p style="margin:0 0 0 0px;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>
+    </div>      
+</div>
+
+<div class="block2">
+	<div class="servicio">
+        <img src="img/block/halitosis.jpg" /> <br />
+        <h3 style="margin:6px 0 0 0;">Halitosis</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>
+    </div>          
+</div>
+
+<div class="block3">
+	<div class="servicio">
+        <img src="img/block/estetica.jpg" /> <br />
+        <h3 style="margin:6px 0 0 0;">Estética Labial</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>
+    </div>          
+</div>         
+
+
+
+
+
+
+
+<div style="clear:both; height:30px;"> </div>
+              
+              
+              
+              
+              
+              
+              
+              
+                
+<div class="block1">
+	<div class="servicio">
+        <img src="img/block/ortodoncia.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Ortodoncia</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>        
+    </div>
+</div>
+
+<div class="block2">
+	<div class="servicio">
+        <img src="img/block/implantes.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Implantes Dentales</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>        
+    </div>         
+</div>
+
+<div class="block3">
+	<div class="servicio">
+        <img src="img/block/terapia.jpg"/> <br />
+        <h3 style="margin:6px 0 0 0;">Terapia Preodontológica</h3>
+        <p style="margin:0 0 0 0;">Lorem ipsum dolor sit amet</p>
+        
+        <div class="servicioText">
+            <p>Lorem ipsum dolor sic amet. Consectetur adespescit</p>
+        </div>
+        
+        <a class="link" href="servicios/ortodoncia/"></a>        
+    </div>        
+</div>
+                                
+                
+                
+
+                
+                
+                
+           
